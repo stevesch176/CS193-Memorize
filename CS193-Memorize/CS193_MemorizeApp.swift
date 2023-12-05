6edc6fce2f29c50446cad7a0fe4c6331420193ae
Steve <stevesch@icloud.ccom>
2023-12-05 15:14:56 -0800

    Initial Commit

---
+++ CS193-Memorize/CS193_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193_MemorizeApp.swift
+//  CS193-Memorize
+//
+//  Created by Steve Schauer on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CS193_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
